0c063220cc438dd8ad97ded5cbc21c2b33cfe2fc
siskaclarissa28 <siskaclarissa28@gmail.com>
2026-07-03 23:55:50 +0700

    Update database configuration to use new environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config(); 
-
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'brooklyn_insurance',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+ host: process.env.MYSQLHOST,
+ user: process.env.MYSQLUSER,
+ password: process.env.MYSQLPASSWORD,
+ database: process.env.MYSQLDATABASE,
+ port: process.env.MYSQLPORT,
+ waitForConnections: true,
+ connectionLimit: 10,
+ queueLimit: 0,
 });
-
 module.exports = db;
